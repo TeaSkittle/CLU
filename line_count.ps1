5c52a67f75d40587a9668c4c3cfe093ff09763bf
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-04-20 16:04:26 -0600

    Update line_count.ps1

--- line_count.ps1
+++ line_count.ps1
@@ -1,3 +1,4 @@
+# This file is used for the remove update function
 $lines=(Get-Content C:\CLU\temp2 | Measure-Object -Line).Lines
 $lines--
 type C:\CLU\temp2 -Tail $lines | Out-File C:\CLU\temp3 -encoding ASCII
