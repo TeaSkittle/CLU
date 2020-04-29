777922625ca46a542d06bd58964e401addbb87b3
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-04-29 15:30:57 -0600

    Update line_count.ps1

--- line_count.ps1
+++ line_count.ps1
@@ -1,4 +1,7 @@
 # This file is used for the remove update function
-$lines=(Get-Content C:\CLU\temp2 | Measure-Object -Line).Lines
+# equivalent to UNIX wc -l
+$lines=(Get-Content C:\1\temp2 | Measure-Object -Line).Lines
+# subtract 1 from lines
 $lines--
-type C:\CLU\temp2 -Tail $lines | Out-File C:\CLU\temp3 -encoding ASCII
+# output file with correct encoding
+type C:\1\temp2 -Tail $lines | Out-File C:\1\temp3 -encoding ASCII
