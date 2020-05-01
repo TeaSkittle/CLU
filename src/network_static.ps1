1e7942a0ff675b05c1108ae7fea4563f45d41974
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-01 15:08:51 -0600

    Create network_static.ps1

---
+++ src/network_static.ps1
@@ -0,0 +1,24 @@
+# Set network settings for static
+
+# Default Variables
+# NOTE: will get from var.ps1 in the future
+
+# Get user input
+ipconfig /all
+$interface = Read-Host "Interface"
+$ip = Read-Host "IP"
+$sub = Read-Host "Subnet Mask"
+$gate = Read-Host "Default Gateway"
+# Defaults if empty input
+if (!$interface) { $interface=$defint }
+if (!$sub) { $sub=$defgate }
+if (!$gate) {$gate=$defsub }
+
+# netsh
+# Set network adapter setting
+netsh interface ipv4 set address name="$interface" static $ip $sub $gate
+netsh interface ipv4 set dns name="$interface" static $dnsone primary
+netsh interface ipv4 add dns name="$interface" addr="$dnstwo" index=2
+
+# Flush DNS
+ipconfig /flushdns
