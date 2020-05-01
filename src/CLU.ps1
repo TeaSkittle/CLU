2a2b3dbd08914de4f91bcd068f6d03879994fbb7
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-01 15:33:46 -0600

    Create CLU.ps1

---
+++ src/CLU.ps1
@@ -0,0 +1,11 @@
+# ==================================================================================
+# Author - Travis Dowd
+# Date Started: 2-7-2020 (v0.1)
+# Version 1.0 completed on: 2-26-2020
+# Powershell version started on: 5-1-2020 (v1.6)
+#
+# Most recent veriosn is always at: https://github.com/TeaSkittle/CLU
+# ==================================================================================
+
+# Load global variables
+. .\var.ps1
