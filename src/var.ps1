1f5c4bc5ae3834be6255e31b7efc4b497282051a
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-01 15:05:40 -0600

    Create var.ps1

---
+++ src/var.ps1
@@ -0,0 +1,31 @@
+# NOTE: Put this at the top of .ps1 files: #Requires -RunAsAdministrator
+# Holds global variables for other scripts
+# run with the "dot" operator: . .\var.ps1
+
+# Title bar
+$ver=1.6
+$title="CLU - $ver"
+$host.ui.RawUI.WindowTitle = “$title”
+
+# Network
+$defint="Ethernet"
+$defsub=255.255.255.0
+$defgate=192.168.9.1
+
+# DNS
+$dnsone=8.8.8.8
+$dnstwo=8.8.4.4
+
+# Time zone
+$deftime="Mountain Standard Time"
+
+# Files
+$dest=C:\CLU\
+
+# Color Scheme
+$bg="black"
+$fg="white"
+# TO change run these:
+# $host.UI.RawUI.BackgroundColor = "$bg"
+# $host.UI.RawUI.ForegroundColor = "$fg"
+# clear
