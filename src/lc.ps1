3c2ef280bc800eba68be4211303822cd18c6ea99
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-03 12:33:00 -0600

    Update lc.ps1

--- src/lc.ps1
+++ src/lc.ps1
@@ -5,3 +5,5 @@ Set-ExecutionPolicy RemoteSigned
 # equivalent to UNIX: wc -l file_name
 $lines=(Get-Content $args[0] | Measure-Object -Line).Lines
 $lines
+
+# NOTE: this does not count empty/blank lines
