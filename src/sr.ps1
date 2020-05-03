98aeef2cf87c6cf8e5374e5cb3a0ae806a7427c7
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-03 12:26:58 -0600

    Update sr.ps1

--- src/sr.ps1
+++ src/sr.ps1
@@ -1,4 +1,7 @@
-# search and replace: sr.ps1 foo bar file_name 
+#Requires -RunAsAdministrator
+Set-ExecutionPolicy RemoteSigned
+
+# search and replace: .\sr.ps1 file_name foo bar 
 # equivalent to UNIX: sed 's/foo/bar/g' file_name
-$sed=(Get-Content -path $args[2]) -replace "$args[0]","$args[1]"
+$sed=(Get-Content -path $args[0]) -replace $args[1],$args[2]
 $sed
