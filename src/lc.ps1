261e1e4d94de5c20201f858164b2a55d83caa886
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-03 12:33:11 -0600

    Update lc.ps1

--- src/lc.ps1
+++ src/lc.ps1
@@ -1,7 +1,7 @@
 #Requires -RunAsAdministrator
 Set-ExecutionPolicy RemoteSigned
 
-# count number of lines: lc.ps1 file_name
+# count number of lines: .\lc.ps1 file_name
 # equivalent to UNIX: wc -l file_name
 $lines=(Get-Content $args[0] | Measure-Object -Line).Lines
 $lines
